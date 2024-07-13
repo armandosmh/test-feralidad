f3192d9c6019ffa15a2aa84a17eb4c928d8b415a
Armando Sobrino <armandosmh@gmail.com>
2024-07-12 21:08:56 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -53,16 +53,22 @@ canvas.addEventListener('click', function(event) {
 
 async function sendEvaluation(nombre, evaluador, x, y, color) {
   const data = {nombre, evaluador, x, y, color};
-  const response = await fetch('https://script.google.com/macros/s/AKfycbwoLafU77absaDMA0kjKIy5lYLOQ8et_ITsZwz6UlWtMGvIrJMWOEpuIr-u_aOK0y9G/exec', {
-    method: 'POST',
-    mode: 'no-cors',  // Asegúrate de manejar CORS si es necesario.
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
+  try {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwoLafU77absaDMA0kjKIy5lYLOQ8et_ITsZwz6UlWtMGvIrJMWOEpuIr-u_aOK0y9G/exec', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Error al enviar la evaluación');
     }
-  });
-  if (!response.ok) {
-    throw new Error('Error al enviar la evaluación');
+    const result = await response.json();
+    console.log('Evaluación enviada con éxito:', result);
+    return result;
+  } catch (error) {
+    console.error('Error al enviar la evaluación:', error);
+    alert('Hubo un problema al enviar la evaluación. Por favor, intenta de nuevo.');
   }
-  return response.json();  // O maneja la respuesta como necesites.
 }
